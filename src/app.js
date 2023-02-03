14a6a551eb1f0575cd86498067aa441384dd19b3
olawale-o <omoogunolawale@gmail.com>
2023-02-03 14:12:03 +0100

    Setup app

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+// const handleError = require('../common/error-handler');
+
+const corsOption = {
+  origin: 'http://localhost:3000',
+  credential: true,
+};
+
+const app = express();
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors(corsOption));
+
+// app.use('/api/v1/task', require('../task/route'));
+
+app.use(async (err) => {
+  console.log(err);
+  // await handleError(err, res);
+});
+
+module.exports = app;
