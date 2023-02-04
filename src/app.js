03650d3a52be9f0f6743264ef702b52abaaaed1c
olawale-o <omoogunolawale@gmail.com>
2023-02-04 11:18:14 +0100

    Fix cors issues

--- src/app.js
+++ src/app.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const swaggerJSDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 // const handleError = require('../common/error-handler');
 
+// const whitelist = ['http://localhost:3000', 'http://localhost:8080'];
+
 const corsOption = {
-  origin: 'http://localhost:3000',
-  credential: true,
+  // origin(origin, callback) {
+  //   if (whitelist.indexOf(origin) !== -1) {
+  //     callback(null, true);
+  //   } else {
+  //     callback(new Error('Not allowed by CORS'));
+  //   }
+  // },
+  origin: true,
+  credentials: true,
+  optionsSuccessStatus: 200,
 };
 
 const app = express();
@@ -16,10 +27,11 @@ const swaggerOptions = require('../swagger-options');
 
 const swaggerSpec = swaggerJSDoc(swaggerOptions);
 
+app.use(cors(corsOption));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors(corsOption));
+app.use(cookieParser());
 
 app.use('/api/v1/account', require('./account'));
 app.use('/api/v1/order_items', require('./order_items'));
