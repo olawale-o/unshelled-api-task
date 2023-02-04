9c4c0457b8252b3303c8bdd69e5284e6240665e7
olawale-o <omoogunolawale@gmail.com>
2023-02-04 02:54:18 +0100

    Create token service

---
+++ src/services/token-service.js
@@ -0,0 +1,34 @@
+const jwt = require('jsonwebtoken');
+
+const signToken = (credentials, secretKey, expiresIn) => new Promise((resolve, reject) => {
+  const { sellerId } = credentials;
+  const options = {
+    expiresIn,
+  };
+  jwt.sign({ sellerId }, secretKey, options, (err, token) => {
+    if (err) {
+      reject(new Error('Forbidden'));
+    } else {
+      resolve(token);
+    }
+  });
+});
+
+const verifyToken = (token, secretKey) => new Promise((resolve, reject) => {
+  jwt.verify(token, secretKey, (err, decoded) => {
+    if (err) {
+      reject(new Error('Forbidden'));
+    } else {
+      resolve(decoded);
+    }
+  });
+});
+const newAccessToken = async (credentials) => signToken(credentials, 'ACCESS_TOKEN_SECRET', '1h');
+
+const newfreshToken = async (credentials) => signToken(credentials, 'REFRESH_TOKEN_SECRET', 1000 * 60 * 60 * 24);
+
+module.exports = {
+  newAccessToken,
+  newfreshToken,
+  verify: async (credentials) => verifyToken(credentials, 'ACCESS_TOKEN_SECRET'),
+};
